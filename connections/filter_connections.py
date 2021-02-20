401520c7d45342d5742fd57dac621012c9056f17
Sam Yong <myong@andrew.cmu.edu>
2021-02-19 19:51:16 -0500

    Code comment for filter connections

--- connections/filter_connections.py
+++ connections/filter_connections.py
@@ -1,4 +1,4 @@
-import db
+import db # refers to ../db.py
 
 connection_as_follower = db.connection_as_follower_count()
 connection_as_following = db.connection_as_following_count()
@@ -6,8 +6,8 @@ print(len(connection_as_follower), 'unique connection among followers')
 print(len(connection_as_following), 'unique connection among followings')
 
 top = lambda n = 0: connection_as_follower[:n] if n > 0 else connection_as_follower
-at_least_as_follower = lambda n = 0: [t for t in connection_as_follower if t[1] >= n]
-at_least_as_following = lambda n = 0: [t for t in connection_as_following if t[1] >= n]
+at_least_as_follower = lambda n = 0: [t for t in connection_as_follower if t[1] >= n] # find connection users who follow at least `n` seed users
+at_least_as_following = lambda n = 0: [t for t in connection_as_following if t[1] >= n] # find connection users who is followed by at least `n` seed users
 
 if __name__ == '__main__':
     import csv
